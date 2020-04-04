1ee20f3019646924495304c93a598450ad6463e0
Mitaka <mitaka001962@gmail.com>
2020-04-04 13:02:35 +0900

    add some files

---
+++ gray_scott_visualizer.js
@@ -0,0 +1,272 @@
+class GrayScottVisualizer {
+    constructor(gl, params) {
+        this.gl = gl;
+        this.params = params;
+
+        this.gl.getExtension('EXT_color_buffer_float');
+        this.gl.getExtension('OES_texture_float_linear');
+
+        const vsSource = 
+        `#version 300 es
+        in vec4 aVertexPosition;
+        in vec2 aTexCoord;
+
+        out vec2 vTexCoord;
+        
+        void main() {
+            gl_Position = aVertexPosition;
+            vTexCoord = aTexCoord;
+        }`;
+
+        const drawFsSource = 
+        `#version 300 es
+        precision highp float;
+        
+        in vec2 vTexCoord;
+
+        out vec4 outColor;
+
+        uniform sampler2D uDrawTex;
+
+        void main() {
+            float r = texture(uDrawTex, vTexCoord).r;
+            // float g = texture(uDrawTex, vTexCoord).g;
+            outColor = vec4(r, r, r, 1.0);
+        }`;
+
+        const updateFsSource =
+        `#version 300 es
+        precision highp float;
+
+        uniform float dx;
+        uniform float dt;
+        uniform float Du;
+        uniform float Dv;
+        uniform float feed;
+        uniform float kill;
+
+        in vec2 vTexCoord;
+
+        out vec2 outState;
+
+        uniform vec2 uTextureSize;
+        uniform sampler2D uUpdateTex;
+
+        void main() {
+            vec2 step = 1.0 / uTextureSize;
+            
+            // calculate laplacian
+            float dec_x = vTexCoord.x - step.x;
+            float inc_x = vTexCoord.x + step.x;
+            float dec_y = vTexCoord.y - step.y;
+            float inc_y = vTexCoord.y + step.y;
+            dec_x = (dec_x < 0.0) ? dec_x + 1.0 : dec_x;
+            inc_x = (inc_x > 1.0) ? inc_x - 1.0 : inc_x;
+            dec_y = (dec_y < 0.0) ? dec_y + 1.0 : dec_y;
+            inc_y = (inc_y > 1.0) ? inc_y - 1.0 : inc_y;
+
+            vec2 uv = texture(uUpdateTex, vTexCoord).rg;
+            vec2 uv1 = texture(uUpdateTex, vec2(dec_x, vTexCoord.y)).rg;
+            vec2 uv2 = texture(uUpdateTex, vec2(inc_x, vTexCoord.y)).rg;
+            vec2 uv3 = texture(uUpdateTex, vec2(vTexCoord.x, dec_y)).rg;
+            vec2 uv4 = texture(uUpdateTex, vec2(vTexCoord.x, inc_y)).rg;
+            vec2 lap_uv = (uv1 + uv2 + uv3 + uv4 - 4.0*uv) / (dx*dx);
+
+            float du = Du*lap_uv.r - uv.r*uv.g*uv.g + feed*(1.0 - uv.r);
+            float dv = Dv*lap_uv.g + uv.r*uv.g*uv.g - uv.g*(feed + kill);
+
+            float u = uv.r + du*dt;
+            u = (u < 0.0) ? 0.0 : u;
+            u = (u > 1.0) ? 1.0 : u;
+            float v = uv.g + dv*dt;
+            v = (v < 0.0) ? 0.0 : v;
+            v = (v > 1.0) ? 1.0 : v;
+
+            outState = vec2(u, v);
+        }`;
+
+        this.drawShaderProgram = this._initShaderProgram(vsSource, drawFsSource);
+        this.updateShaderProgram = this._initShaderProgram(vsSource, updateFsSource);
+        this.drawProgramLocations = {
+            'attribute': {
+                'aVertexPosition': this.gl.getAttribLocation(this.drawShaderProgram, "aVertexPosition"),
+                'aTexCoord': this.gl.getAttribLocation(this.drawShaderProgram, "aTexCoord"),
+            },
+            'uniform': {
+                'uDrawTex': this.gl.getUniformLocation(this.drawShaderProgram, "uDrawTex"),
+            }
+        }
+        this.updateProgramLocations = {
+            'attribute': {
+                'aVertexPosition': this.gl.getAttribLocation(this.updateShaderProgram, "aVertexPosition"),
+                'aTexCoord': this.gl.getAttribLocation(this.updateShaderProgram, "aTexCoord"),
+            },
+            'uniform': {
+                'dx': this.gl.getUniformLocation(this.updateShaderProgram, "dx"),
+                'dt': this.gl.getUniformLocation(this.updateShaderProgram, "dt"),
+                'Du': this.gl.getUniformLocation(this.updateShaderProgram, "Du"),
+                'Dv': this.gl.getUniformLocation(this.updateShaderProgram, "Dv"),
+                'feed': this.gl.getUniformLocation(this.updateShaderProgram, "feed"),
+                'kill': this.gl.getUniformLocation(this.updateShaderProgram, "kill"),
+                'uTextureSize': this.gl.getUniformLocation(this.updateShaderProgram, "uTextureSize"),
+                'uUpdateTex': this.gl.getUniformLocation(this.updateShaderProgram, "uUpdateTex"),
+            }
+        };        
+        this.vao = this._initVertexArray();
+        this.textures = new Array(2);
+        for (let i = 0; i < this.textures.length; i++) {
+            this.textures[i] = this._loadTexture(null);
+        }
+        this.framebuffer = this.gl.createFramebuffer();
+    }
+
+    draw(step=8) {
+        this.gl.bindVertexArray(this.vao);
+
+        // update
+        for (let i = 0; i < step; i++) {
+            this.gl.useProgram(this.updateShaderProgram);
+
+            this.gl.bindTexture(this.gl.TEXTURE_2D, this.textures[1]);
+            this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.framebuffer);
+            this.gl.framebufferTexture2D(this.gl.FRAMEBUFFER, this.gl.COLOR_ATTACHMENT0,
+                                        this.gl.TEXTURE_2D, this.textures[1], 0);
+
+            this.gl.activeTexture(this.gl.TEXTURE1);
+            this.gl.bindTexture(this.gl.TEXTURE_2D, this.textures[0]);
+            this.gl.uniform1i(this.updateProgramLocations.uniform.uUpdateTex, 1);
+
+            this._setUpdateProgramUniforms();
+
+            this.gl.viewport(0, 0, this.params.width, this.params.height);
+            this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+            this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+            this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
+
+            // swap read and write textures
+            this.textures = [this.textures[1], this.textures[0]];
+        }
+        
+        // draw
+        this.gl.useProgram(this.drawShaderProgram);
+
+        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+
+        this.gl.activeTexture(this.gl.TEXTURE0);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.textures[1]);
+        this.gl.uniform1i(this.drawProgramLocations.uniform.uDrawTex, 0);
+
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, 4);
+    }
+
+    setTexture(source) {
+        this.gl.bindTexture(this.gl.TEXTURE_2D, this.textures[0]);
+        this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RG32F, this.params.width, this.params.height, 0, 
+                            this.gl.RG, this.gl.FLOAT, source);
+        this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+    }
+
+    _loadShader(type, source) {
+        const shader = this.gl.createShader(type);
+        this.gl.shaderSource(shader, source);
+        this.gl.compileShader(shader);
+
+        if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+            alert('failed to compile a shader: ' + this.gl.getShaderInfoLog(shader));
+            this.gl.deleteShader(shader);
+            return null;
+        }
+
+        return shader;
+    }
+
+    _initShaderProgram(vsSource, fsSource) {
+        const vertexShader = this._loadShader(this.gl.VERTEX_SHADER ,vsSource);
+        const fragmentShader = this._loadShader(this.gl.FRAGMENT_SHADER ,fsSource);
+
+        const shaderProgram = this.gl.createProgram();
+        this.gl.attachShader(shaderProgram, vertexShader);
+        this.gl.attachShader(shaderProgram, fragmentShader);
+        this.gl.linkProgram(shaderProgram);
+
+        if (!this.gl.getProgramParameter(shaderProgram, this.gl.LINK_STATUS)) {
+            alert('failed to initialize a shader program: ' + this.gl.getProgramInfoLog(shaderProgram));
+            this.gl.deleteProgram(shaderProgram);
+            return null;
+        }
+    
+        return shaderProgram;
+    }
+
+    _initVertexArray() {
+        const vao = this.gl.createVertexArray();
+        this.gl.bindVertexArray(vao);
+
+        const positionBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array([
+            -1.0,  1.0, 
+            -1.0, -1.0,
+             1.0,  1.0,
+             1.0, -1.0,
+        ]), this.gl.STATIC_DRAW);
+        this.gl.enableVertexAttribArray(this.drawProgramLocations.attribute.aVertexPosition);
+        this.gl.vertexAttribPointer(
+            this.drawProgramLocations.attribute.aVertexPosition,
+            2, this.gl.FLOAT, false, 0, 0);
+        this.gl.enableVertexAttribArray(this.updateProgramLocations.attribute.aVertexPosition);
+        this.gl.vertexAttribPointer(
+            this.updateProgramLocations.attribute.aVertexPosition,
+            2, this.gl.FLOAT, false, 0, 0);
+
+        const texCoordBuffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, texCoordBuffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array([
+            0.0, 1.0,
+            0.0, 0.0,
+            1.0, 1.0,
+            1.0, 0.0
+        ]), this.gl.STATIC_DRAW);
+        this.gl.enableVertexAttribArray(this.drawProgramLocations.attribute.aTexCoord);
+        this.gl.vertexAttribPointer(
+            this.drawProgramLocations.attribute.aTexCoord,
+            2, this.gl.FLOAT, false, 0, 0);
+        this.gl.enableVertexAttribArray(this.updateProgramLocations.attribute.aTexCoord);
+        this.gl.vertexAttribPointer(
+            this.updateProgramLocations.attribute.aTexCoord,
+            2, this.gl.FLOAT, false, 0, 0);
+
+        this.gl.bindVertexArray(null);
+        return vao;
+    }
+
+    _loadTexture(source) {
+        const texture = this.gl.createTexture();
+        this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+        this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RG32F, this.params.width, this.params.width,
+                           0, this.gl.RG, this.gl.FLOAT, source);
+        
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.LINEAR);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+        this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+
+        this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+
+        return texture;
+    }
+
+    _setUpdateProgramUniforms() {
+        this.gl.uniform1f(this.updateProgramLocations.uniform.dx, this.params.dx);
+        this.gl.uniform1f(this.updateProgramLocations.uniform.dt, this.params.dt);
+        this.gl.uniform1f(this.updateProgramLocations.uniform.Du, this.params.Du);
+        this.gl.uniform1f(this.updateProgramLocations.uniform.Dv, this.params.Dv);
+        this.gl.uniform1f(this.updateProgramLocations.uniform.feed, this.params.feed);
+        this.gl.uniform1f(this.updateProgramLocations.uniform.kill, this.params.kill);
+        this.gl.uniform2f(this.updateProgramLocations.uniform.uTextureSize,
+                          this.params.width, this.params.height);
+    }
+}
---
+++ main.js
@@ -0,0 +1,117 @@
+$(function() {
+    let request_id = null;
+    const presets = [
+        { feed: 0.022, kill: 0.051 },   // stripe                                  
+        { feed: 0.035, kill: 0.065 },   // spots
+        { feed: 0.012, kill: 0.050 },   // wandering bubbles
+        { feed: 0.025, kill: 0.050 },   // waves
+        { feed: 0.040, kill: 0.060 },   // amorphous
+        { feed: 0.028, kill: 0.054 },   // bumps
+        { feed: 0.025, kill: 0.060 },   // waving spots
+        { feed: 0.030, kill: 0.060 },   // snapping strings
+        { feed: 0.011, kill: 0.046 }    // balloons
+    ]
+    const default_preset = 0;
+    const params = {
+        'width': 512,
+        'height': 512,
+        'dx': 0.01,
+        'dt': 1,
+        'Du': 2e-5,
+        'Dv': 1e-5,
+        'feed': presets[default_preset].feed,
+        'kill': presets[default_preset].kill,
+    };
+
+    const gl = document.getElementById("canvas").getContext("webgl2");
+    if (!gl) {
+        alert("Unable to initialize WebGL2. Make sure your browser or machine supports it.");
+        return;
+    }
+    const visualizer = new GrayScottVisualizer(gl, params);
+
+    // initialize u, v
+    const createInitTexture = function() {
+        const SQUARE_SIZE = 40;
+        const x_start = Math.floor(params.width / 2) - Math.floor(SQUARE_SIZE / 2);
+        const y_start = Math.floor(params.height / 2) - Math.floor(SQUARE_SIZE / 2);
+        const x_end = x_start + SQUARE_SIZE;
+        const y_end = y_start + SQUARE_SIZE;
+    
+        let uvTexture = [];
+        for (let i = 0; i < params.height; i++) {
+            for (let j = 0; j < params.width; j++) {
+                let u, v;
+                if ((i >= y_start) && (i < y_end) && (j >= x_start) && (j < x_end)) {
+                    u = 0.5 + Math.random() * 0.1;
+                    v = 0.25 + Math.random() * 0.1;
+                } else {
+                    u = 1.0;
+                    v = 0.0;
+                }
+                uvTexture.push(u, v);
+            }
+        }
+    
+        return new Float32Array(uvTexture);
+    }
+
+    const render = function() {
+        visualizer.draw();
+        request_id = requestAnimationFrame(render);
+    }
+
+    const initUI = function() {
+        $("#init_btn").button().click(function () {
+            if (request_id === null) {
+                $("#ctrl_btn").text("停止");
+            } else {
+                cancelAnimationFrame(request_id);
+                request_id = null;
+            }
+            visualizer.setTexture(createInitTexture());
+            request_id = requestAnimationFrame(render);
+        });
+        $("#init_btn").text("初期化");
+
+        $("#ctrl_btn").button().click(function () {
+            if (request_id === null) {
+                request_id = requestAnimationFrame(render);
+                $("#ctrl_btn").text("停止");
+            } else {
+                cancelAnimationFrame(request_id);
+                request_id = null;
+                $("#ctrl_btn").text("再開");
+            }
+        });
+        $("#ctrl_btn").text("停止");
+
+        $("#preset_select").change(function (event) {
+            $("#feed_slider").slider("value", presets[event.target.value].feed);
+            $("#kill_slider").slider("value", presets[event.target.value].kill);
+        });
+        $("#preset_select").val(default_preset);
+
+        $("#feed_slider").slider({
+            value: params.feed, min: 0.000, max: 0.050, step: 0.001,
+            change: function (event, ui) {params.feed = ui.value;
+                                          $("#feed_span").text(params.feed.toFixed(3));},
+            slide: function (event, ui) {params.feed = ui.value;
+                                         $("#feed_span").text(params.feed.toFixed(3));}
+        });
+        $("#feed_slider").slider("value", params.feed);
+
+        $("#kill_slider").slider({
+            value: params.kill, min: 0.040, max: 0.075, step: 0.001,
+            change: function (event, ui) {params.kill = ui.value;
+                                          $("#kill_span").text(params.kill.toFixed(3));},
+            slide: function (event, ui) {params.kill = ui.value;
+                                         $("#kill_span").text(params.kill.toFixed(3));}
+        });
+        $("#kill_slider").slider("value", params.kill);
+    }
+
+    initUI();
+    visualizer.setTexture(createInitTexture())
+    request_id = requestAnimationFrame(render);
+});
